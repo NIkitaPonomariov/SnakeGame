8933dfb93939745d7739d5a879bc87788c543e94
Nikita <ponomariovnikita02@gmail.com>
2024-03-17 21:21:30 +0200

    create the main classes for game

---
+++ game/classes.py
@@ -0,0 +1,17 @@
+"""
+here we gonna create all our classes(snake,movement and something like this)
+"""
+
+class Game:
+    def __init__(self):
+        pass
+
+
+class Snake:
+    def __init__(self):
+        pass
+
+class Fruit:
+    def __init__(self):
+        pass
+
---
+++ game/main.py
@@ -0,0 +1,5 @@
+"""
+here we gonna run all our files,functions and play game
+"""
+import pygame as pG
+from random import randint
